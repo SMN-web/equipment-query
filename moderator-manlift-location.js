a12b74811f5d4163874df76da0c64544013998c9
SMN-web <nafil.8895.s@gmail.com>
2025-09-21 11:36:50 +0300

    Create moderator-manlift-location.js

---
+++ moderator-manlift-location.js
@@ -0,0 +1,8 @@
+export function showManliftLocation(container) {
+  container.innerHTML = `
+    <div class="demo-card">
+      <h3>Manlift Location Subsection</h3>
+      <p class="demo-txt">Demo content for Manlift Location.</p>
+    </div>
+  `;
+}
