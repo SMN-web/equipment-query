c51aeb3ca803d823192760e4f21ffad50f52dbb1
SMN-web <nafil.8895.s@gmail.com>
2025-09-21 11:35:55 +0300

    Create moderator-crane-location.js

---
+++ moderator-crane-location.js
@@ -0,0 +1,8 @@
+export function showCraneLocation(container) {
+  container.innerHTML = `
+    <div class="demo-card">
+      <h3>Crane Location Subsection</h3>
+      <p class="demo-txt">Demo content for Crane Location.</p>
+    </div>
+  `;
+}
