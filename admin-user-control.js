5ad79227d0c2df7412daeb6f8e3c13d5548514af
SMN-web <nafil.8895.s@gmail.com>
2025-09-21 11:33:55 +0300

    Create admin-user-control.js

---
+++ admin-user-control.js
@@ -0,0 +1,8 @@
+// admin-user-control.js
+export function showUserControl(container) {
+  container.innerHTML = `
+    <h3>🧑‍💼 Modify + Delete Users</h3>
+    <div id="user-control-list"><p>Loading users...</p></div>
+  `;
+  // Logic for user list loading...
+}
