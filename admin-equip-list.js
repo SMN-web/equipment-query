51608725ba8e5f3abf4f45ae0187abbae80a86db
SMN-web <nafil.8895.s@gmail.com>
2025-09-21 11:46:04 +0300

    Create admin-equip-list.js

---
+++ admin-equip-list.js
@@ -0,0 +1,9 @@
+export function showEquipList(container) {
+  container.innerHTML = `
+    <div class="demo-card">
+      <h3>📋 Equipment List</h3>
+      <div id="manage-table-container">Equipment table and filters go here.</div>
+      <p class="demo-txt">Paginated/searchable list to be added.</p>
+    </div>
+  `;
+}
