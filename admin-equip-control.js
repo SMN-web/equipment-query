f3405b2f873a41154aa5f3d2fb8f9042dab08bdc
SMN-web <nafil.8895.s@gmail.com>
2025-09-21 11:46:40 +0300

    Create admin-equip-control.js

---
+++ admin-equip-control.js
@@ -0,0 +1,8 @@
+export function showEquipControl(container) {
+  container.innerHTML = `
+    <div class="demo-card">
+      <h3>⚙ Equipment Control</h3>
+      <p class="demo-txt">Controls for equipment (activate, deactivate, special actions) go here.</p>
+    </div>
+  `;
+}
