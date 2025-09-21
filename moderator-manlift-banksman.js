8a317f7ba0678cf284a8f9f83484778b5893483b
SMN-web <nafil.8895.s@gmail.com>
2025-09-21 11:37:11 +0300

    Create moderator-manlift-banksman.js

---
+++ moderator-manlift-banksman.js
@@ -0,0 +1,8 @@
+export function showManliftBanksman(container) {
+  container.innerHTML = `
+    <div class="demo-card">
+      <h3>Manlift Banksman Subsection</h3>
+      <p class="demo-txt">Demo content for Manlift Banksman.</p>
+    </div>
+  `;
+}
