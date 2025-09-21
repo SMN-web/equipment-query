cd86b8fa413a095a79b45b0c542f2061ffa692bb
SMN-web <nafil.8895.s@gmail.com>
2025-09-21 11:47:20 +0300

    Create admin-rigging-details.js

---
+++ admin-rigging-details.js
@@ -0,0 +1,8 @@
+export function showRiggingDetails(container) {
+  container.innerHTML = `
+    <div class="demo-card">
+      <h3>📄 Rigging Details</h3>
+      <p class="demo-txt">Rigging details management tools will appear here.</p>
+    </div>
+  `;
+}
