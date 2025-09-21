5af93ad4f0ab29d29be79862df6a2231858bc1ee
SMN-web <nafil.8895.s@gmail.com>
2025-09-21 11:36:14 +0300

    Create moderator-crane-rigger.js

---
+++ moderator-crane-rigger.js
@@ -0,0 +1,8 @@
+export function showCraneRigger(container) {
+  container.innerHTML = `
+    <div class="demo-card">
+      <h3>Crane Rigger Subsection</h3>
+      <p class="demo-txt">Demo content for Crane Rigger.</p>
+    </div>
+  `;
+}
