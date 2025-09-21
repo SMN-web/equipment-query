a5c94de3f74fb896eb24655567d8441a12db53e1
SMN-web <nafil.8895.s@gmail.com>
2025-09-21 11:46:20 +0300

    Create admin-equip-edit.js

---
+++ admin-equip-edit.js
@@ -0,0 +1,9 @@
+export function showEquipEdit(container) {
+  container.innerHTML = `
+    <div class="demo-card">
+      <h3>✏️ Edit Equipment</h3>
+      <div id="edit-table-container">Editable equipment table will appear here.</div>
+      <p class="demo-txt">Inline editing, bulk actions can be implemented here.</p>
+    </div>
+  `;
+}
